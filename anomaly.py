a73abb11a9cc6cd17918e50a7465d3da4a822579
barbbuettner <barbara.hopfinger@gmx.de>
2025-11-27 09:51:45 +0100

    Expand with additional functionalities
    Refactor AnomalyScore class to AnomalyScorer and introduce AnomalyEncoder for enhanced data preprocessing and anomaly detection.

--- anomaly.py
+++ anomaly.py
@@ -1,6 +1,9 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.decomposition import TruncatedSVD
+from sklearn.neural_network import MLPRegressor
 
-class AnomalyScore():
+class AnomalyScorer():
   def __init__(self, lift_weight=0.2, kl_weight=0.2, gini_weight=0.2, support_weight=0.2, tfidf_weight=0.2, min_score=0.0):
     tot_weight = lift_weight + kl_weight + gini_weight + support_weight + tfidf_weight
     if tot_weight > 0:
@@ -69,13 +72,78 @@ class AnomalyScore():
     final_score = np.where(final_score >= self.min_score, final_score, 0.0)
     return final_score
 
-def fit_transform(self, X, y, dates):
-  X = np.array(X)
-  X_transformed = []
-  for i in range(X.shape[1]):
-    x = X[:,i]
-    X_transformed.append(self._calculate_full_score_for_binned_array(x, y, dates))
-  X_transformed = np.array(X_transformed).T
-  return X_transformed
+  def fit_transform(self, X, y, dates):
+    X = np.array(X)
+    X_transformed = []
+    for i in range(X.shape[1]):
+      x = X[:,i]
+      X_transformed.append(self._calculate_full_score_for_binned_array(x, y, dates))
+    X_transformed = np.array(X_transformed).T
+    return X_transformed
     
 
+class AnomalyEncoder():
+  def __init__(self, n_svd_components, anomaly_scorer, hidden_layer_sizes=(128, 16, 128)):
+    self.n_svd_components = n_svd_components
+    self.anomaly_scorer = anomaly_scorer
+    self.hidden_layer_sizes = hidden_layer_sizes
+    
+  def preprocess(self, X, default_num=0.0, default_cat='', exclusion_columns=[]):
+    num_cols = []
+    cat_cols = []
+    cols = [col for col in X.columns if col not in exclusion_columns]
+    for col in cols:
+      try:
+        X[col] = X[col].astype(float)
+        num_cols.append(col)
+        X[col] = X[col].fillna(default_num)
+      except:
+        X[col] = X[col].fillna(default_cat)
+        cat_cols.append(col)
+        X[col] = X[col].astype(str)
+    if len(num_cols) > 0:
+      if not hasattr(self, 'scaler'):
+        scaler = StandardScaler()
+        x_num = scaler.fit_transform(X[num_cols])
+        self.scaler = scaler
+      else:
+        x_num = self.scaler.transform(X[self.scaler.feature_names_in_])
+    if len(cat_cols) > 0:
+      if not hasattr(self, 'ohe'):
+        ohe = OneHotEncoder(sparse_output = False, min_frequency=0.001, handle_unknown='infrequent_if_exist')
+        x_cat = ohe.fit_transform(X[cat_cols])
+        self.ohe = ohe
+      else:
+        x_cat = self.ohe.transform(X[self.ohe.feature_names_in_])
+    if hasattr(self, 'scaler'):
+      if hasattr(self, 'ohe'):
+        x_prep = np.hstack((x_num, x_cat))
+      else:
+        x_prep = x_num
+    else:
+      if hasattr(self, 'ohe'):
+        x_prep = x_cat
+      else:
+        print("Could not continue preprocessing")
+        raise
+    if not hasattr(self, 'svd'):
+      svd = TruncatedSVD(self.n_svd_components)
+      x_svd = svd.fit_transform(x_prep)
+      self.svd = svd
+    else:
+      x_svd = svd.transform(x_prep)
+    return x_svd
+
+  def train_encoder(self, X):
+    x_prep = self.preprocess(X)
+    x_anom = self.anomaly_scorer.fit_transform(X)
+    encoder = MLPRegressor(hidden_layer_sizes=self.hidden_layer_sizes)
+    encoder.fit(x_prep, x_anom)
+    self.encoder = encoder
+    return x_prep, x_anom
+
+  def train_approximate_scorer(self, x_prep, x_anom, hidden_layer_sizes=(128,)):
+    scorer_network = MLPRegressor(hidden_layer_sizes=hidden_layer_sizes)
+    scorer_network.fit(x_prep, x_anom)
+    self.approximate_scorer = scorer_network
+    
